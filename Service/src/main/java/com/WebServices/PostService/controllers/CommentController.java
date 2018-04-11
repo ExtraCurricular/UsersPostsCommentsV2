cf6ec4daa27e68f04e0219c79c451ebbb241b94f
ExtraCurricular <ExtraCurricular@github.com>
2018-04-11 23:08:31 +0300

    .

--- Service/src/main/java/com/WebServices/PostService/controllers/CommentController.java
+++ Service/src/main/java/com/WebServices/PostService/controllers/CommentController.java
@@ -34,7 +34,7 @@ public class CommentController {
 
     @GetMapping("/comments")
     public String getAllComments() {
-        final String uri = "193.219.91.103:2740/locations";
+        final String uri = "0.0.0.0:5000/locations";
 
         return new RestTemplate().getForObject(uri, String.class);
     }
